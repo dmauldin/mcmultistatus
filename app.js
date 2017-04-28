90f6eddcad2cd749d2687817276ec0229f10cb62
Dave Mauldin <dmauldin@gmail.com>
2017-04-28 11:16:46 -0700

    removed unused stuff

--- app.js
+++ app.js
@@ -2,27 +2,10 @@ var async = require('async'),
     mcping = require('mc-ping-updated'),
     _ = require('underscore'),
     express = require('express'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
     app = express();
-
-mongoose.connect('mongodb://localhost:27017/wheee');
 
 app.set('view engine', 'jade');
 app.use('/public', express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
-    next();
-});
-
-app.use(morgan('dev'));
-
 
 
 app.get('/', function(req, res) {
@@ -54,21 +37,5 @@ app.get('/', function(req, res) {
         }
     });
 });
-
-app.route('/login')
-    .get(function(req, res) {
-        res.send("login form goes here");
-    })
-    .post(function(req, res) {
-        res.send("processing the login form");
-    });
-
-var apiRouter = express.Router();
-
-apiRouter.get('/', function(req, res) {
-    res.json({message: "hey, it's an api!"});
-});
-
-app.use('/api', apiRouter);
 
 app.listen(3000);
